13655c5ada20a75beca274393c88dc55afeb2a2e
pkedia009 <pkedia009@gmail.com>
2024-04-04 12:55:11 +0530

    Update c5-02-eks-outputs.tf

--- 01-ekscluster-terraform-manifests/c5-02-eks-outputs.tf
+++ 01-ekscluster-terraform-manifests/c5-02-eks-outputs.tf
@@ -67,7 +67,7 @@ output "node_group_public_version" {
   value       = aws_eks_node_group.eks_ng_public.version
 }
 */
-
+/*
 # EKS Node Group Outputs - Private
 
 output "node_group_private_id" {
@@ -90,3 +90,25 @@ output "node_group_private_version" {
   value       = aws_eks_node_group.eks_ng_private.version
 }
 
+*/
+# EKS Node Group Outputs - gpu nodes
+
+output "node_group_private_id" {
+  description = "Node Group 1 ID"
+  value       = aws_eks_node_group.eks_ng_private.id
+}
+
+output "node_group_private_arn" {
+  description = "Private Node Group ARN"
+  value       = aws_eks_node_group.eks_ng_private.arn
+}
+
+output "node_group_private_status" {
+  description = "Private Node Group status"
+  value       = aws_eks_node_group.eks_ng_private.status 
+}
+
+output "node_group_private_version" {
+  description = "Private Node Group Kubernetes Version"
+  value       = aws_eks_node_group.eks_ng_private.version
+}

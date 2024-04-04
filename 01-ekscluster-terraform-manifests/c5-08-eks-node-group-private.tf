445c7b845cdf6a518f2f1ee6098e6af519dc64e6
pkedia009 <pkedia009@gmail.com>
2024-04-04 13:32:46 +0530

    Update c5-08-eks-node-group-private.tf

--- 01-ekscluster-terraform-manifests/c5-08-eks-node-group-private.tf
+++ 01-ekscluster-terraform-manifests/c5-08-eks-node-group-private.tf
@@ -1,5 +1,5 @@
 
-/*
+
 # Create AWS EKS Node Group - Private
 resource "aws_eks_node_group" "eks_ng_private" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
@@ -44,5 +44,5 @@ resource "aws_eks_node_group" "eks_ng_private" {
   }
 }
 
-*/
+
 
